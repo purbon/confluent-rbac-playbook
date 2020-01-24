9b7865f3a0bceec71d03c28be2f6e34aa8b5e743
Pere Urbon <pere@confluent.io>
2020-01-24 17:19:44 +0100

    Fix proper kafka cluster id

--- create-roles.sh
+++ create-roles.sh
@@ -1,15 +1,70 @@
 #!/usr/bin/env bash
 
+################################## GET KAFKA CLUSTER ID ########################
+ZK_CONTAINER=zookeeper
+ZK_PORT=2181
+echo "Retrieving Kafka cluster id from docker-container '$ZK_CONTAINER' port '$ZK_PORT'"
+KAFKA_CLUSTER_ID=$(docker exec -it $ZK_CONTAINER zookeeper-shell localhost:$ZK_PORT get /cluster/id 2> /dev/null | grep \"version\" | jq -r .id)
+if [ -z "$KAFKA_CLUSTER_ID" ]; then
+    echo "Failed to retrieve kafka cluster id from zookeeper"
+    exit 1
+fi
+
 ## Login into MDS
 XX_CONFLUENT_USERNAME=professor XX_CONFLUENT_PASSWORD=professor confluent login --url http://localhost:8090
 
+SUPER_USER=professor
+SUPER_USER_PASSWORD=professor
+SUPER_USER_PRINCIPAL="User:$SUPER_USER"
 
 ## Create Service Roles
 CONNECT_PRINCIPAL="User:fry"
 C3_PRINCIPAL="User:hermes"
-KAFKA_CLUSTER_ID="x64IAgb0TfOs-3-YoGB4gA"
+SR_PRINCIPAL="User:leela"
+#KAFKA_CLUSTER_ID="x64IAgb0TfOs-3-YoGB4gA"
 CONNECT=connect-cluster
+SR=schema-registry
+C3=c3-cluster
 
+################################### SETUP SUPERUSER ###################################
+echo "Creating Super User role bindings"
+
+confluent iam rolebinding create \
+    --principal $SUPER_USER_PRINCIPAL  \
+    --role SystemAdmin \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+confluent iam rolebinding create \
+    --principal $SUPER_USER_PRINCIPAL \
+    --role SystemAdmin \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --schema-registry-cluster-id $SR
+
+confluent iam rolebinding create \
+    --principal $SUPER_USER_PRINCIPAL \
+    --role SystemAdmin \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --connect-cluster-id $CONNECT
+
+################################### SCHEMA REGISTRY ###################################
+echo "Creating Schema Registry role bindings"
+
+# SecurityAdmin on SR cluster itself
+confluent iam rolebinding create \
+    --principal $SR_PRINCIPAL \
+    --role SecurityAdmin \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID \
+    --schema-registry-cluster-id $SR
+
+# ResourceOwner for groups and topics on broker
+for resource in Topic:_schemas Group:schema-registry
+do
+    confluent iam rolebinding create \
+        --principal $SR_PRINCIPAL \
+        --role ResourceOwner \
+        --resource $resource \
+        --kafka-cluster-id $KAFKA_CLUSTER_ID
+done
 
 ################################### CONNECT ###################################
 echo "Creating Connect role bindings"
@@ -28,7 +83,7 @@ declare -a ConnectResources=(
     "Topic:connect-status"
     "Group:connect-cluster"
     "Group:secret-registry"
-    "Topic:_secrets"
+    "Topic:_confluent-secrets"
 )
 for resource in ${ConnectResources[@]}
 do
@@ -43,4 +98,19 @@ done
 echo "Creating C3 role bindings"
 
 # C3 only needs SystemAdmin on the kafka cluster itself
-confluent iam rolebinding create --principal $C3_PRINCIPAL --role SystemAdmin --kafka-cluster-id $KAFKA_CLUSTER_ID
+confluent iam rolebinding create \
+    --principal $C3_PRINCIPAL \
+    --role SystemAdmin \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID
+
+
+
+echo "Finished setting up role bindings"
+echo "    kafka cluster id: $KAFKA_CLUSTER_ID"
+echo "    connect cluster id: $CONNECT"
+echo "    schema registry cluster id: $SR"
+echo
+echo "    super user account: $SUPER_USER_PRINCIPAL"
+echo "    connect service account: $CONNECT_PRINCIPAL"
+echo "    schema registry service account: $SR_PRINCIPAL"
+echo "    C3 service account: $C3_PRINCIPAL"    
